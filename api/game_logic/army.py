870cba2695d0d0c83bf346ebd2260a5e5d4171f4
Kushagra-Pant <1pantkus@gmail.com>
2026-09-06 12:24:10 -0400

    Added several checks in deploy corresponding to game logic

--- api/game_logic/army.py
+++ api/game_logic/army.py
@@ -20,31 +20,67 @@ def getUnitData(gameData, unittype):
         return allunits[domain][unit]
   return None
 
+def getDomain(gameData, unittype):
+  allunits = gameData.getDefaultGameData()["Units"]
+  for domain in allunits:
+    if unittype in allunits[domain]:
+      return domain
+  return None
+  
+
 def deployUnit(gameData, nation, territory, unit, quantity):
+    if not isinstance(quantity, int):
+        raise ValueError("Use an integer!")
+
+    if quantity == 0:
+        raise ValueError("You can't deploy 0 troops")
+
+    if quantity < 0:
+        raise ValueError("You can't deploy negative troops")
+
+    # Territory ownership
     if territory["Nation"] != nation["Name"]:
-        raise ValueError(
-            f'{territory["Name"]} is owned by {territory["Nation"]}'
-        )
+        raise ValueError(f'{territory["Name"]} is owned by {territory["Nation"]}')
 
+    # Territory integration
+    if territory["Integrated"] > gameData.gameTime():
+        raise ValueError("This territory has not been integrated yet")
+
+    # Find unit
     unitdata = getUnitData(gameData, unit)
-
     if unitdata is None:
         raise ValueError("Invalid Unit Type")
 
-    if unit not in nation["Inventory"]:
-        raise ValueError(
-            f"Nation does not have any {unit}"
-        )
+    inventoryName = unitdata
 
-    if nation["Inventory"][unit] < quantity:
-        raise ValueError(
-            f"Nation does not have enough {unit}"
-        )
+    # if unitdata["Each"] > 1:
+    #     inventoryName += " Division"
+    if inventoryName not in nation["Inventory"]:
+        raise ValueError(f"Nation does not have any {inventoryName}")
 
-    unitid = quadify(gameData.incrementUnitCounters(unitdata["Short Form"])) + unitdata["Short Form"]
+    if nation["Inventory"][inventoryName] < quantity:
+        raise ValueError(f"Nation does not have enough {inventoryName}")
 
+    unitDomain = getDomain(gameData, unitdata)
+    
+    
+    if unitDomain == "Naval":
+        # Coast occupant logic will be implemented later
+        pass
+
+    if unitDomain == "Air":
+        if "Airport" not in territory["Buildings"]:
+            raise ValueError(f'{territory["Name"]} must have an airport for you to deploy an aircraft')
+
+
+    # Generate unit ID
+    shortForm = unitdata["Short Form"]
+
+    unitid = quadify(gameData.incrementUnitCounters(shortForm)) + shortForm
+
+    # Remove units from inventory
     inventory = nation["Inventory"].copy()
-    inventory[unit] -= quantity
+    inventory[inventoryName] -= quantity
 
     gameData.updateNation(
         nation["Name"],
@@ -53,19 +89,24 @@ def deployUnit(gameData, nation, territory, unit, quantity):
         }
     )
 
+    # Ground units start active
+    active = unitDomain == "Ground"
+
+    # Create deployed unit
     gameData.createUnit({
         "Name": unitid,
-        "Type": unit,
+        "Type": unitdata,
         "Quantity": quantity * unitdata["Each"],
         "Nation": nation["Name"],
         "Location": territory["Name"],
-        "Active": True,
+        "Active": active,
         "TiredUntil": 0
     })
 
+
     return {
         "unit": unitid,
-        "type": unit,
+        "type": unitdata,
         "quantity": quantity * unitdata["Each"],
         "location": territory["Name"]
     }
